cfe75a0ea19fcfae9d857b53eea8da9f79b6eaf0
Richard Adem <richy486@gmail.com>
2020-03-06 19:11:38 +0000

    split out

--- TestSwiftUI2/ContentView.swift
+++ TestSwiftUI2/ContentView.swift
@@ -8,18 +8,31 @@
 
 import SwiftUI
 
-//struct Restaurant: Identifiable {
-//    var id = UUID()
-//    var name: String
-//}
-//
-//struct RestaurantRow: View {
-//    var restaurant: Restaurant
-//
-//    var body: some View {
-//        Text(restaurant.name)
-//    }
-//}
+struct Restaurant: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+struct RestaurantRow: View {
+  var restaurant: Restaurant
+  var num: Int
+
+  var body: some View {
+    VStack {
+      GeometryReader { geo in
+        Rectangle()
+          .fill(LinearGradient(gradient: Gradient(colors: [.yellow, .red]),
+                               startPoint: .topLeading,
+                               endPoint: .bottomTrailing))
+          .frame(width: 300, height: 200)
+          .clipShape(RoundedRectangle(cornerRadius: 20))
+          .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).minX-120) / 8),
+                            axis: (x: 0, y: 1, z: 0))
+      }
+    }
+    .frame(width: 180)
+  }
+}
 //
 //struct ContentView: View {
 //    var body: some View {
@@ -39,19 +52,7 @@ struct ContentView: View {
     ScrollView(.horizontal, showsIndicators: false) {
       HStack(spacing: 90) {
         ForEach(1..<10) { num in
-          VStack {
-            GeometryReader { geo in
-              Rectangle()
-                .fill(LinearGradient(gradient: Gradient(colors: [.yellow, .red]),
-                                     startPoint: .topLeading,
-                                     endPoint: .bottomTrailing))
-                .frame(width: 300, height: 200)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .rotation3DEffect(.degrees(-Double(geo.frame(in: .global).minX-120) / 8),
-                                  axis: (x: 0, y: 1, z: 0))
-            }
-          }
-          .frame(width: 180)
+          RestaurantRow(restaurant: Restaurant(name: "Joe's Original"), num: num)
         }
       }
       .padding(120)
